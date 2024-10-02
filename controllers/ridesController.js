bedc6b5735c1a7b570171075df2c90e69a927992
maker-jws <ws.joshua@gmail.com>
2024-10-02 13:33:50 -0600

    add basic error handling tests, create index show and edit pages

--- controllers/ridesController.js
+++ controllers/ridesController.js
@@ -18,17 +18,39 @@ router.post('/', cleanRequestBody, async (req, res) => {
 })
 
 // INDEX
-router.get('/', (req, res) => res.send('rides index route'))
+router.get('/', async (req, res) => {
+  try {
+    const allPublicRides = await Ride.find({ isPublic: true })
+    console.log(allPublicRides)
+    res.render('rides/index', { rides: allPublicRides })
+  } catch (err) {
+    res.redirect('/?errorMsg=create-error')
+  }
+})
 // -- ICEBOX: if auth -> send only filtered results else only send public responses
 
 // SHOW -
-router.get('/:rideId', (req, res) => res.send('rides show route'))
+router.get('/:rideId', async (req, res) => {
+  try {
+    const foundRide = await Ride.findById(req.params.rideId)
+    res.render('rides/show', { ride: foundRide })
+  } catch (err) {
+    res.redirect('/?errorMsg=create-error')
+  }
+})
 
 // DELETE
 router.delete('/:rideId', (req, res) => res.send('rides delete route'))
 
 // EDIT -
-router.get('/:rideId/edit', (req, res) => res.send('rides edit route'))
+router.get('/:rideId/edit', async (req, res) => {
+  try {
+    const rideToEdit = await Ride.findById(req.params.rideId)
+    res.render('rides/edit', { ride: rideToEdit })
+  } catch (err) {
+    res.redirect('/?errorMsg=create-error')
+  }
+})
 
 // UPDATE
 router.put('/:rideId', (req, res) => res.send('rides update route'))
